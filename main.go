5096bb7ba6aa9bcb4ecc10c3864d99357da7a860
xiabee <xiabee@foxmail.com>
2023-12-05 22:19:55 +0800

    fix simulate bugs

--- main.go
+++ main.go
@@ -10,51 +10,61 @@ import (
 var simulating = false
 
 func main() {
+	// Register a mouse event for the middle button
 	hook.Register(hook.MouseHold, []string{}, func(e hook.Event) {
 		if e.Button == hook.MouseMap["center"] {
 			simulating = !simulating
 			if simulating {
 				log.Default().Println("Simulating starts")
-				go simulateMouseMovementAndKeyPress()
+				// Start mouse movement and key press simulations in separate goroutines
+				go simulateMouseMovement()
+				go simulateKeyPress()
 			} else {
 				log.Default().Println("Simulating ends")
 			}
 		}
 	})
 
+	// Start listening for mouse and keyboard events
 	s := hook.Start()
 	defer hook.End()
 
+	// Process events
 	<-hook.Process(s)
 }
 
-func simulateMouseMovementAndKeyPress() {
+func simulateMouseMovement() {
 	for simulating {
 		// Get the current mouse position
 		x, y := robotgo.Location()
 
-		// Move the mouse quickly horizontally, keeping the vertical position unchanged
-		newX := x + 500 // Each move adds 500 pixels
-
+		// Move the mouse horizontally while keeping the vertical position unchanged
+		newX := x + 500 // Move 500 pixels each time
 		robotgo.Move(newX, y)
 
-		// Press Q and E every three seconds
-		select {
-		case <-time.After(3 * time.Second):
-			err := robotgo.KeyTap("e")
-			if err != nil {
-				return
-			}
-			log.Default().Println("E pressed")
-			time.Sleep(100 * time.Millisecond) // 短暂延迟
-			err = robotgo.KeyTap("q")
-			if err != nil {
-				return
-			}
-			log.Default().Println("Q pressed")
-		default:
-			// Control loop speed to allow response to user's mouse movements
-			time.Sleep(10 * time.Millisecond)
+		// Control the speed of the loop
+		time.Sleep(10 * time.Millisecond)
+	}
+}
+
+func simulateKeyPress() {
+	for simulating {
+		// Press Q and E keys every three seconds
+		time.Sleep(3 * time.Second)
+		err := robotgo.KeyTap("e")
+		if err != nil {
+			log.Default().Println("Error pressing E:", err)
+			return
 		}
+		log.Default().Println("E pressed")
+
+		time.Sleep(100 * time.Millisecond) // Short delay
+
+		err = robotgo.KeyTap("q")
+		if err != nil {
+			log.Default().Println("Error pressing Q:", err)
+			return
+		}
+		log.Default().Println("Q pressed")
 	}
 }
